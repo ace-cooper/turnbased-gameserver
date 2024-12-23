6fd054f4b484bc0411cd80b705f44be83c485af8
Hugo Fagundes <hugo.c.fagundes@gmail.com>
2024-12-23 20:06:15 -0300

    feat: add Redis connection pooling and enhance cache management

--- src/core/cache.ts
+++ src/core/cache.ts
@@ -1,13 +1,27 @@
 import * as crypto from 'crypto';
-import { RedisClientType, createClient } from 'redis';
+import { Redis } from 'ioredis';
 import { cacheAddresses } from './config';
-
+import * as genericPool from 'generic-pool';
+import { getCtx } from './context';
+import Redlock, { Settings, Lock } from 'redlock';
 export namespace Cache {
     const hashRing: Map<number, Node> = new Map();
+    const poolingNodes: Map<string, genericPool.Pool<Redis>> = new Map();
+    const connections: Map<string, Redis> = new Map();
+    const redlock: Redlock = new Redlock([cacheAddresses[0]]);
+    
+    const redisFactory = (host) => ({
+        create: async () => {
+          return new Redis(host);
+        },
+        destroy: async (client) => {
+         await client.quit();
+        }
+    } as genericPool.Factory<Redis>);
     
     export interface Node {
         id: string;
-        client: RedisClientType
+        client?: Redis;
     }
 
     export const KEY_SYMBOL = ":";
@@ -30,6 +44,11 @@ export namespace Cache {
         const position = hash(node.id);
         hashRing.delete(position);
     }
+
+    export const getNodeForEntityAndKey = async (entityName: string, entityKey: string) => {
+        const key = Cache.normalizeKey(entityName, entityKey);
+        return Cache.getNodeForKey(key);
+    }
     
     export const getNodeForKey = async (key: string): Promise<Node | null> => {
         if (hashRing.size === 0) {
@@ -48,14 +67,107 @@ export namespace Cache {
             }
         }
 
-        if (!node.client.isOpen) {
-            await node.client.connect();
+        node.client = await getSubClient(node.id);
+        if (node.client.status !== 'ready') {
+            try {
+            // await node.client.connect(); ?
+            } catch(e) {
+                console.log('Error connecting', e);
+            }
         }
 
         return node;
     }
 
-    for (const address of cacheAddresses) {
-        Cache.addNode({ id: address, client: createClient(address) });
+    export const getDefaultHost = () => cacheAddresses[0];
+    export const getSubClient = async (host = getDefaultHost()) => {
+
+        const connection = connections.get(host);
+        if (connection && ['ready'].includes(connection.status)) {
+            return connection;
+        } else if (connection) {
+     
+            // try { await connection.quit(); } catch(e) {  }
+        }
+        const pool = poolingNodes.get(host)
+        const acquired = await pool!.acquire(1);
+        connections.set(host, acquired);
+        redlock.clients.add(acquired);
+        return acquired;
+
+    };
+
+    export const closeAll = async () => {
+        for (const node of hashRing.values()) {
+            const pnode = await poolingNodes.get(node.id)!;
+            redlock.clients.delete(node.client);
+            pnode.drain();
+        }
     }
+
+    export const release = async (node: Node & { client: Redis }) => {
+        try {
+            await poolingNodes.get(node.id)!.release(node.client);
+            redlock.clients.delete(node.client);
+        } catch(e) {
+            // console.log('Error releasing', e);
+            await node.client?.quit();
+        }
+    }
+
+
+
+    for (const address of cacheAddresses) {
+
+        if (!poolingNodes.has(address)) {
+            poolingNodes.set(address, genericPool.createPool(redisFactory(address), { max: 5, min: 1 }));
+        }
+        Cache.addNode({ id: address });
+    }
+
+    export const getCtxClient = (): Redis => {
+        const ctx = getCtx();
+        return ctx?.get("cacheClient");
+    };
+
+    export const withClient = async (block: (client: Redis) => any, key: { id: string; target: string; }) => {
+        const ctx = getCtx();
+        const client = ctx?.get("cacheClient");
+        if (client) {
+            return block(client);
+        } else {
+
+        
+            try {
+                const clientKey = Cache.normalizeKey(key.target, key.id);
+                const node = await Cache.getNodeForKey(clientKey);
+                ctx.set("cacheClient", node.client);
+                return await block(ctx.get('cacheClient'));
+            } catch (e) {
+                console.log(e);
+            } 
+        }
+    };
+
+    export const lock = async (resources: string[], duration: number, settings?: Settings) => await redlock.acquire(resources, duration, settings);
+
+    export const withLock = async (options: {resources: string[], duration: number, settings: Settings}, block: <T = any>(data?: Lock)=>Promise<T>) => {
+        const _lock =  await Cache.lock(options.resources, options.duration, options.settings);
+        try {
+            return await block(_lock);
+        } finally {
+            await _lock.release();
+        }
+    }
+
+}
+
+export function UseCacheClient(id: string, target: string) {
+    return function (_target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        const method = descriptor.value;
+
+        descriptor.value = async function (...args: any[]) {
+            return Cache.withClient(() => method(...args), { id, target });
+        };
+    };
 }
--- src/core/database.ts
+++ src/core/database.ts
@@ -1,26 +1,59 @@
-import { Prisma, PrismaClient } from '@prisma/client';
+import { Prisma as ORM, PrismaClient } from '@prisma/client';
 import { getCtx } from './context';
 import { ulid } from 'ulid';
 import { Cache } from './cache';
+import { devMode } from './config';
+
+
+export { Prisma as ORM, PrismaClient as ORMClient }from '@prisma/client';
+
+interface SetOptions {
+  EX?: number;
+  EXAT?: number;
+}
+
+let client: PrismaClient;
 
 export namespace Database {
 
     export const genId = (seedTime?: number) => ulid(seedTime);
     export const genVr = (seedTime?: number) => ulid(seedTime);
 
-    export const createDBM = () => new PrismaClient()
+    export const createDBM = async () => {
+      if (!client) {
+        client = new PrismaClient(devMode ?{
+            log: [
+                {
+                    emit: 'stdout',
+                    level: 'query',
+                    
+                },
+            ],
+        } : null)
+       await client.$connect();
+
+      }
+      return client;
+  }
 
     export const getDBM: () => PrismaClient = () => getCtx()?.get("dbm") as PrismaClient;
+    export const getCM: () => Cache.Node = () => getCtx()?.get("sub") as Cache.Node;
 
-    export const withDBM = async (block: (dbm: PrismaClient) => any) => {
-        const ctx = getCtx();
-        const dbm = ctx?.get("dbm");
-        if (dbm) {
-        return block(dbm);
-        } else {
-        const dbm = createDBM();
-        
-        await dbm.$connect();
+    export const withDBM = async (block: (dbm: PrismaClient) => any, options?: { cacheDisabled?: boolean }) => {
+      const ctx = getCtx();
+      const dbm = ctx?.get("dbm");
+      if (dbm) {
+      return block(dbm);
+      } else {
+        const dbm = await createDBM();
+
+        if (!options?.cacheDisabled) {
+          const sub = getCM() || {
+            id: Cache.getDefaultHost(),
+            client: await Cache.getSubClient()
+          };
+          ctx.set("sub", sub);
+        }
 
         ctx.set("dbm", dbm);
         
@@ -28,77 +61,113 @@ export namespace Database {
             return await block(ctx.get('dbm'));
         } catch (e) {
             console.log(e);
+            throw e;
         } finally {
-            await dbm.$disconnect();
+            // await dbm.$disconnect();
+            // await Cache.release(sub as any);
         }
-        }
-    };
+     }
+  };
 
-    export async function withTransaction(block) {
-        const ctx = getCtx();
-        const dbm = ctx?.get("dbm") as PrismaClient;
-        if (!dbm) {
-            throw new Error("Transaction Error: The context must has a db manager");
-        }
+  export async function withTransaction<T>(block: (transaction) => Promise<T>, options?: {
+    maxWait?: number;
+    timeout?: number;
+    isolationLevel?: ORM.TransactionIsolationLevel;
+  }): Promise<T> {
+      const ctx = getCtx();
+      const dbm = ctx?.get("dbm") as PrismaClient;
+      if (!dbm) {
+          throw new Error("Transaction Error: The context must has a db manager");
+      }
 
-        const transaction = ctx.get("transaction");
+      const transaction = ctx.get("transaction");
 
-        if (transaction) {
-        return await block(transaction);
-        } else {
-        return await dbm.$transaction(async (transaction) => {
+      if (transaction) {
+      return await block(transaction);
+      } else {
+      return await dbm.$transaction<T>(async (transaction) => {
 
-            ctx.set("transaction", transaction);
-            try {
+          ctx.set("transaction", transaction);
+          try {
             return await block(transaction);
-            } catch(e) {
-            console.log(e)
-            } finally {
-            ctx.set("transaction", null);
-            }
-        })
-        }
-    }
+          } catch(e) {
+            if (devMode) console.log(e)
+            throw e;
+          } finally {
+          ctx.set("transaction", null);
+          }
+      }, options)
+      }
+  }
 
-    export const getCurrentTransaction = () => getCtx()?.get("transaction");
+  export const getCurrentTransaction = () => getCtx()?.get("transaction");
 
-    export const getCurrentDBMX = () => (getCurrentTransaction() || getDBM())  as PrismaClient;
-
-    export const getCtxModels = () => getCtx()?.get('dbm')?.models;
+  export const getCurrentDBMX = () => (getCurrentTransaction() || getDBM())  as PrismaClient;
+  
+  export const getCtxModels = () => getCtx()?.get('dbm')?.models;
 
     export abstract class BaseEntity {
         protected static _repo;
         public static get repo(): any { throw new Error(`${this.name} must override repo getter`); }
+        public get cacheExport(): any { return this }
+        public static get canonical(): any { throw new Error(`${this.name} must override canonical getter`); }
+
         id: string;
-        createdAt: Date;
-        updatedAt: Date;
+        created_at: string | Date | number;
+        updated_at: string | Date | number;
         active: boolean;
         deleted: boolean;
         version: string;
     }
 
+    interface BaseEntityConstructor<T extends BaseEntity> {
+      new (): T;
+      repo: any;
+    }
     export class BaseService<T extends BaseEntity> {
-        constructor(public readonly target: T) {}
-      
-        public get repository() {
-          return this.target['repository'];
+        protected cacheEnabled: boolean = false;
+        protected cacheEx: number = 600;
+
+        protected get cacheOptions(): SetOptions {
+          return this.cacheEx ? {
+            EX: this.cacheEx,
+          } : null
         }
 
-        public static factory<E extends BaseService<C>, C extends BaseEntity>(entityClass: { new(target: C): E }): E {
-          return new entityClass(new (this as any as {new(): C})());
+        constructor(public readonly target: BaseEntityConstructor<T>) {}
+      
+        public get repository() {
+          return this.target.repo;
+        }
+
+        public get dbm() {
+          return getCurrentDBMX();
+        }
+
+        public cacheKey(id: string, targetName: string = this.target['name']) {
+          return Cache.normalizeKey(targetName, id);
+        }
+
+        public factory(attributes: Partial<T> | T): T {
+          const instance = new this.target();
+          Object.assign(instance, attributes);
+          return instance;
+        }
+
+        public async getCacheClient(key: string) {
+          return await Cache.getNodeForKey(key);
+        }
+
+        public async getCacheClientByKey(id: string, targetName: string = this.target['name']) {
+          return await this.getCacheClient(this.cacheKey(id, targetName));
         }
       
         public async findById(id: string, bypassCache?: boolean): Promise<T> {
           try {
-            if (!bypassCache) {
-                const key = Cache.normalizeKey(this.target['name'], id);
-                const cache = await Cache.getNodeForKey(key);
-
+            if (!bypassCache && this.cacheEnabled) {
+                const cache = await this.getCache(id);
                 if (cache) {
-                    const data = await cache.client.get(key);
-                    if (data) {
-                        return JSON.parse(data);
-                    }
+                    return this.factory(cache);
                 }
             }
           } catch (e) {
@@ -115,52 +184,187 @@ export namespace Database {
         public async create(data: Partial<T>): Promise<T> {
             data.id = data.id || genId();
             data.version = data.version || genVr();
+            data.active = data.active ?? true;
+            data.deleted = data.deleted ?? false;
   
             const entity = await this.repository.create({ data });
             try {
-                const key = Cache.normalizeKey(this.target['name'], entity.id);
-                const cache = await Cache.getNodeForKey(key);
-                await cache?.client.set(key, JSON.stringify(entity));
+                if (this.cacheEnabled) {
+                    const instantiated = this.factory(entity)
+                    const key = Cache.normalizeKey(this.target['name'], entity.id);
+                    const cache = await Cache.getNodeForKey(key);
+                    await cache?.client.setex(key, this.cacheOptions.EX, JSON.stringify(instantiated.cacheExport));
+                }
             } catch (e) {
                 console.log(e);
             }
-            return entity;
+            return !!entity ? this.factory(entity) : null;
+        }
+
+        public async delete(id: string) {
+          return this.repository.update({
+            where: {
+              id,
+            },
+            data: {
+              deleted: true,
+              active: false,
+              updated_at: new Date(),
+            },
+          });
         }
       
       
         public async optimisticUpdate<U extends Partial<T | any>>(
           entity: T,
-          data: Prisma.XOR<T, U>
-        ): Promise<Prisma.BatchPayload> {
-          data = {
-            rowVersion: genVr(),
-            updatedAt: new Date(),
+          data: ORM.XOR<T, U>
+        ): Promise<ORM.BatchPayload> {
+          const updated_at = new Date();
+          const queryData = {
+            version: {
+              increment: 1
+            },
+            updated_at,
             ...data,
           };
 
-          const result: Prisma.BatchPayload = await this.repository.updateMany({
+          const result: ORM.BatchPayload = await this.repository.updateMany({
             where: {
               id: entity.id,
               version: entity.version,
             },
-            data,
+            data: queryData,
           });
 
           if (result.count > 0) {
             try {
-                const key = Cache.normalizeKey(this.target['name'], entity.id);
-                const cache = await Cache.getNodeForKey(key);
-                const cacheEntity = JSON.parse(await cache?.client.get(key) || '{}');
-                await cache?.client.set(key, JSON.stringify({
-                    ...cacheEntity,
-                    ...data
-                }));
+                if (this.cacheEnabled) {
+                  const instantiated = this.factory({
+                    ...entity,
+                    updated_at,
+                    ...data,
+                    version: genVr()
+                  })
+                  const key = Cache.normalizeKey(this.target['name'], entity.id);
+                  const cache = await Cache.getNodeForKey(key);
+                  const cacheEntity = JSON.parse(await cache?.client.get(key) || '{}');
+                  await cache?.client.setex(key, this.cacheOptions.EX, JSON.stringify({
+                      ...cacheEntity,
+                      ...instantiated.cacheExport
+                  }));
+                }
             } catch (e) {
                 console.log(e);
             }
           }
 
           return result;
+        }
+
+        protected async updateEntityCache(entity: T) {
+          try {
+              if (this.cacheEnabled) {
+                await this.updateCache(this.target['name'], entity.id, entity)
+              }
+          } catch (e) {
+              console.log(e);
+          }
+        }
+
+
+        protected async setEntityCache(entity: T) {
+          try {
+              if (this.cacheEnabled) {
+                await this.setCache(this.target['name'], entity.id, entity)
+              }
+          } catch (e) {
+              console.log(e);
+          }
+        }
+
+        protected async updateCache(entityName: string, entityKey: string, data: any, cacheOptions?: SetOptions) {
+          const key = Cache.normalizeKey(entityName, entityKey);
+          const cache = await Cache.getNodeForKey(key);
+          const cacheEntity = JSON.parse(await cache?.client.get(key) || '{}');
+          await cache?.client.setex(key, cacheOptions?.EX || this.cacheOptions?.EX, JSON.stringify({
+              ...cacheEntity,
+              ...data
+          }));
+        }
+        protected async setCache(entityName: string, entityKey: string, data: any, cacheOptions?: SetOptions) {
+          const key = Cache.normalizeKey(entityName, entityKey);
+          
+          const cache = await Cache.getNodeForKey(key);
+          try {
+          if (cacheOptions?.EXAT) {
+            
+            await cache?.client.set(key, JSON.stringify({
+              ...data
+            }));
+           
+            await cache?.client.expireat(key, cacheOptions?.EXAT);
+          } else {
+            await cache?.client.setex(key, cacheOptions?.EX || this.cacheOptions?.EX, JSON.stringify({
+                ...data
+            }));
+          }
+          } catch(e) {
+            // console.log(e); 
+            throw e;
+          } 
+        }
+
+        protected async getCache(id: string, targetName: string = this.target['name']) {
+          const key = Cache.normalizeKey(targetName, id);
+          const cache = await Cache.getNodeForKey(key);
+
+          if (cache) {
+              const data = await cache.client.get(key);
+              if (data) {
+                  return JSON.parse(data);
+              }
+          }
+        }
+
+        protected async releaseCache(id: string, targetName: string = this.target['name']) {
+          const key = Cache.normalizeKey(targetName, id);
+          const cache = await Cache.getNodeForKey(key);
+          await cache.client.del(key);
+        }
+
+        protected async upsert(data: Partial<T>): Promise<T> {
+          data.id = data.id || genId();
+          data.version = data.version || genVr();
+          data.active = data.active ?? true;
+          data.deleted = data.deleted ?? false;
+          const entity = await this.repository.upsert({
+            where: {
+              id: data.id,
+              version: {
+                lte: data.version
+              }
+            },
+            update: {
+              ...data,
+              version: {
+                increment: 1
+              },
+              updated_at: new Date(),
+            },
+            create: data,
+          });
+
+          try {
+              if (this.cacheEnabled) {
+                  const instantiated = this.factory(entity)
+                  const key = Cache.normalizeKey(this.target['name'], entity.id);
+                  const cache = await Cache.getNodeForKey(key);
+                  await cache?.client.setex(key, this.cacheOptions.EX, JSON.stringify(instantiated.cacheExport));
+              }
+          } catch (e) {
+              console.log(e);
+          }
+          return !!entity ? this.factory(entity) : null;
         }
       }
 
